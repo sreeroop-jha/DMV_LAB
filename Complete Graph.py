71c13afa224f42d26aa18f85a541090dd536550c
sreeroop-jha <sreeroopjhan0703@gmail.com>
2026-02-23 12:00:16 +0530

    Add files via upload
    Complete Graph using networkx library and matplotlib

---
+++ Complete Graph.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+n = int(input("Enter the number of vertices: "))
+if(n>3):
+    G = nx.complete_graph(n)
+    nx.draw(G, node_color = 'green' , node_size = 1500, with_labels = True)
+    plt.show()
+
+else:
+    print("Try Again!")
+            
